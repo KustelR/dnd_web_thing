8e74d92e0546758268984bb638788393f9bc0256
KustelR <dmitriysstudios2@gmail.com>
2024-12-18 13:28:06 +0400

    created navbar

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -6,8 +6,9 @@ export default async function Header(props: { lng: string }) {
   const { lng } = props;
   const { t } = await useTranslation(lng, undefined);
   return (
-    <header>
-      <h1>{t("title")}</h1>
+    <header className="p-2 bg-emerald-700 bg-opacity-10">
+      <h1 className="font-bold">{t("title")}</h1>
+      <Nav lng={lng}></Nav>
     </header>
   );
 }
