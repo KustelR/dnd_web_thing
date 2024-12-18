e8923b847609619c991c0b2694f7e4e6875b51aa
KustelR <dmitriysstudios2@gmail.com>
2024-12-18 13:29:16 +0400

    runned prettier

--- frontend/public/locales/automation.ts
+++ frontend/public/locales/automation.ts
@@ -1,54 +1,78 @@
-import * as fs from 'fs/promises';
-import * as path from 'path';
-
+import * as fs from "fs/promises";
+import * as path from "path";
 
 async function mirrorTranslations() {
-    
-    const localesPath = `./public/locales`;
-    
-    const mainLocale = "en";
+  const localesPath = `./public/locales`;
 
-    const localeDirectories = await fs.readdir(path.join(localesPath), { withFileTypes: true });
-    const mirrorTargets = localeDirectories.filter(dir => dir.isDirectory() && dir.name !== mainLocale);
+  const mainLocale = "en";
 
-    console.log("mirroring new locale inputs...")
+  const localeDirectories = await fs.readdir(path.join(localesPath), {
+    withFileTypes: true,
+  });
+  const mirrorTargets = localeDirectories.filter(
+    (dir) => dir.isDirectory() && dir.name !== mainLocale,
+  );
 
-    const files = await fs.readdir(path.join(localesPath, mainLocale));
+  console.log("mirroring new locale inputs...");
 
-    files.forEach(async (file) => {
-        const origin = await fs.readFile(path.join(localesPath, mainLocale, file), { encoding: 'utf-8'});
-        
-        mirrorTargets.forEach(async (target) => {
-            try {
-                await fs.access(path.join(localesPath, target.name, file), fs.constants.F_OK);
+  const files = await fs.readdir(path.join(localesPath, mainLocale));
 
-                const originObject = JSON.parse(origin);
-                const targetObject = JSON.parse(await fs.readFile(path.join(localesPath, target.name, file), { encoding: 'utf-8'}));
-                console.log(Object.entries(originObject), targetObject);
-                Object.keys(originObject).forEach((key) => {
-                    if (originObject.hasOwnProperty(key) && targetObject.hasOwnProperty(key)) {}
-
-                    else if (originObject.hasOwnProperty(key) && !targetObject.hasOwnProperty(key)) {
-                        console.log(`[UPDATED] ${target.name}/${file} ${key}`);
-                        targetObject[key] = originObject[key];
-                    }
-                    else {
-                        throw new Error(`Key ${key} exists in ${target.name} locale, but not in master locale`);
-                    }
-                });
-
-                await fs.writeFile(path.join(localesPath, target.name, file), JSON.stringify(targetObject, null, 2), { encoding: 'utf-8'});
-            } catch (err) {
-                if (err == undefined  || !err.hasOwnProperty("code")) throw err;
-                //@ts-ignore checked for property above. should not crash with obscure information
-                if (err.code != 'ENOENT') throw err;
-                    
-                console.log(`[NOT FOUND] ${target.name}/${file} locale does not exist. Creating new`)
-                fs.writeFile(path.join(localesPath, target.name, file), origin, {flag: 'as+'});
-            }
-        });
+  files.forEach(async (file) => {
+    const origin = await fs.readFile(path.join(localesPath, mainLocale, file), {
+      encoding: "utf-8",
     });
-    
+
+    mirrorTargets.forEach(async (target) => {
+      try {
+        await fs.access(
+          path.join(localesPath, target.name, file),
+          fs.constants.F_OK,
+        );
+
+        const originObject = JSON.parse(origin);
+        const targetObject = JSON.parse(
+          await fs.readFile(path.join(localesPath, target.name, file), {
+            encoding: "utf-8",
+          }),
+        );
+        console.log(Object.entries(originObject), targetObject);
+        Object.keys(originObject).forEach((key) => {
+          if (
+            originObject.hasOwnProperty(key) &&
+            targetObject.hasOwnProperty(key)
+          ) {
+          } else if (
+            originObject.hasOwnProperty(key) &&
+            !targetObject.hasOwnProperty(key)
+          ) {
+            console.log(`[UPDATED] ${target.name}/${file} ${key}`);
+            targetObject[key] = originObject[key];
+          } else {
+            throw new Error(
+              `Key ${key} exists in ${target.name} locale, but not in master locale`,
+            );
+          }
+        });
+
+        await fs.writeFile(
+          path.join(localesPath, target.name, file),
+          JSON.stringify(targetObject, null, 2),
+          { encoding: "utf-8" },
+        );
+      } catch (err) {
+        if (err == undefined || !err.hasOwnProperty("code")) throw err;
+        //@ts-ignore checked for property above. should not crash with obscure information
+        if (err.code != "ENOENT") throw err;
+
+        console.log(
+          `[NOT FOUND] ${target.name}/${file} locale does not exist. Creating new`,
+        );
+        fs.writeFile(path.join(localesPath, target.name, file), origin, {
+          flag: "as+",
+        });
+      }
+    });
+  });
 }
 
 mirrorTranslations();
--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Link from 'next/link';
-import { useTranslation } from '@/app/i18n';
+import React from "react";
+import Link from "next/link";
+import { useTranslation } from "@/app/i18n";
 
-export default async function nav(props: {lng: string}) {
-    const {lng} = props;
-    const {t} = await useTranslation(lng, undefined);
+export default async function nav(props: { lng: string }) {
+  const { lng } = props;
+  const { t } = await useTranslation(lng, undefined);
 
-    const liClassName = 'hover:bg-opacity-10';
+  const liClassName = "hover:bg-opacity-10";
 
-    return (
-    <nav className=''>
-        <ul className='flex [&>*]:mr-2 [&>*]:px-1 [&>*]:bg-emerald-800 [&>*]:bg-opacity-0'>
-            <li className={liClassName}>
-                <Link href={`/${lng}/`}>{t('nav_home')}</Link>
-            </li>
-            <li className={liClassName}>
-                <Link href={`/${lng}/play`}>{t('nav_play')}</Link>
-            </li>
-            <li className={liClassName}>
-                <Link href={`/${lng}/master`}>{t('nav_master')}</Link>
-            </li>
-        </ul>
+  return (
+    <nav className="">
+      <ul className="flex [&>*]:mr-2 [&>*]:px-1 [&>*]:bg-emerald-800 [&>*]:bg-opacity-0">
+        <li className={liClassName}>
+          <Link href={`/${lng}/`}>{t("nav_home")}</Link>
+        </li>
+        <li className={liClassName}>
+          <Link href={`/${lng}/play`}>{t("nav_play")}</Link>
+        </li>
+        <li className={liClassName}>
+          <Link href={`/${lng}/master`}>{t("nav_master")}</Link>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
